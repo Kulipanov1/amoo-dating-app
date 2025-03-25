6b2e51ed4ea3a52c79f75ea2785c0f86a1756bb0
AlekivaTU <your.email@example.com>
2025-03-25 21:59:09 +0300

    Fix deployment configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
-  
-  // Customize the config before returning it.
-  config.output = {
-    ...config.output,
-    publicPath: '/amoo-dating-app/'
-  };
+  const config = await createExpoWebpackConfigAsync({
+    ...env,
+    babel: {
+      dangerouslyAddModulePathsToTranspile: ['@ui-kitten/components']
+    }
+  }, argv);
 
-  return config;
+  return {
+    ...config,
+    output: {
+      ...config.output,
+      publicPath: '/amoo-dating-app/'
+    }
+  };
 }; 
