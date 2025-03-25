92a481e805c134462e4becfd0fc7f0db0d0bb7e8
AlekivaTU <your.email@example.com>
2025-03-25 22:19:47 +0300

    Complete overhaul of deployment configuration

---
+++ app.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  name: "Amoo Dating App",
+  slug: "amoo-dating-app",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff"
+  },
+  assetBundlePatterns: [
+    "**/*"
+  ],
+  ios: {
+    supportsTablet: true
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff"
+    }
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+    bundler: "webpack",
+    output: "static",
+    publicPath: "/amoo-dating-app/"
+  }
+}; 
