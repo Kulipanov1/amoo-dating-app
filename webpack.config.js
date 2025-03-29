5df2d658c76ecab364b5d7f90ba3abfcc58fecae
AlekivaTU <your.email@example.com>
2025-03-29 13:10:51 +0300

    Fix webpack configuration and build settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,21 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
+  const config = await createExpoWebpackConfigAsync({
+    ...env,
+    babel: {
+      dangerouslyAddModulePathsToTranspile: ['@react-native']
+    }
+  }, argv);
+
+  // Customize the config before returning it.
+  config.resolve = {
+    ...config.resolve,
+    alias: {
+      ...config.resolve.alias,
+      '../Utilities/Platform': '@react-native/Libraries/Utilities/Platform'
+    }
+  };
+
   return config;
 }; 
