2b13228eca5a03b1fe72c47778f3650e8e038236
AlekivaTU <your.email@example.com>
2025-03-29 13:59:03 +0300

    изменение 5

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,9 @@ module.exports = async function (env, argv) {
   
   config.resolve.alias = {
     ...config.resolve.alias,
-    'react-native-gesture-handler': 'react-native-web',
+    'react-native$': 'react-native-web',
+    'react-native/Libraries/ReactPrivate/ReactNativePrivateInterface.web': 'react-native-web/dist/modules/ReactNativePrivateInterface',
+    'react-native/Libraries/Utilities/Platform': 'react-native-web/dist/exports/Platform',
   };
 
   return config;
