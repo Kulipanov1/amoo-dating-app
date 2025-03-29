3f316ff16f53f2c1d63005d8d61db0d96ccbdf4b
AlekivaTU <your.email@example.com>
2025-03-29 14:03:16 +0300

    измен6

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,26 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const path = require('path');
 
 module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
+  const config = await createExpoWebpackConfigAsync({
+    ...env,
+    babel: {
+      dangerouslyAddModulePathsToTranspile: [
+        '@react-native',
+        'react-native-gesture-handler',
+      ],
+    },
+  }, argv);
   
   config.resolve.alias = {
     ...config.resolve.alias,
     'react-native$': 'react-native-web',
-    'react-native/Libraries/ReactPrivate/ReactNativePrivateInterface.web': 'react-native-web/dist/modules/ReactNativePrivateInterface',
-    'react-native/Libraries/Utilities/Platform': 'react-native-web/dist/exports/Platform',
+    'react-native-gesture-handler': path.resolve(__dirname, './node_modules/react-native-web/dist/vendor/react-native/Touchable'),
+    '@react-native': 'react-native-web',
+    'react-native/Libraries/Components/View/ViewStylePropTypes': 'react-native-web/dist/exports/View/ViewStylePropTypes',
+    'react-native/Libraries/EventEmitter/RCTDeviceEventEmitter': 'react-native-web/dist/vendor/react-native/NativeEventEmitter/RCTDeviceEventEmitter',
+    'react-native/Libraries/vendor/emitter/EventEmitter': 'react-native-web/dist/vendor/react-native/emitter/EventEmitter',
+    'react-native/Libraries/vendor/emitter/EventSubscriptionVendor': 'react-native-web/dist/vendor/react-native/emitter/EventSubscriptionVendor',
   };
 
   return config;
