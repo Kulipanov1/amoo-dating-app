c545312d1bd1dd294eb2c955de5fd79c8e7f5003
AlekivaTU <your.email@example.com>
2025-03-29 13:54:36 +0300

    изменение 4

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,5 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      'react-native-reanimated/plugin',
-      'babel-plugin-react-native-web',
-      '@babel/plugin-proposal-export-namespace-from',
-    ],
   };
 }; 
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,11 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
-const path = require('path');
 
 module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync({
-    ...env,
-    babel: {
-      dangerouslyAddModulePathsToTranspile: [
-        '@react-native',
-        'react-native-reanimated',
-        'react-native-gesture-handler',
-      ],
-    },
-  }, argv);
-
-  // Add aliases for native modules
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  
   config.resolve.alias = {
     ...config.resolve.alias,
-    'react-native$': 'react-native-web',
-    'react-native-reanimated': 'react-native-web/dist/vendor/reanimated',
-    'react-native-gesture-handler': 'react-native-web/dist/vendor/react-native-gesture-handler',
-  };
-
-  // Ensure proper fallbacks
-  config.resolve.fallback = {
-    ...config.resolve.fallback,
-    'react-native/Libraries/Utilities/codegenNativeCommands': false,
-    'react-native/Libraries/Components/View/ViewNativeComponent': false,
-    'react-native/Libraries/Image/AssetRegistry': false,
-    crypto: require.resolve('crypto-browserify'),
-    stream: require.resolve('stream-browserify'),
-    path: require.resolve('path-browserify'),
+    'react-native-gesture-handler': 'react-native-web',
   };
 
   return config;
