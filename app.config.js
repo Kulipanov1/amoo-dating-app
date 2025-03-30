2c56aba649370234b7b73d5ef405a80582becec5
AlekivaTU <your.email@example.com>
2025-03-30 21:30:17 +0300

    тест

--- app.config.js
+++ app.config.js
@@ -1,15 +1,9 @@
-module.exports = {
-  name: 'Amoo One',
+export default {
+  name: 'Amoo one',
   slug: 'amoo-one',
   version: '1.0.0',
   orientation: 'portrait',
-  icon: './assets/icon.png',
   userInterfaceStyle: 'light',
-  splash: {
-    image: './assets/splash.png',
-    resizeMode: 'contain',
-    backgroundColor: '#8A2BE2'
-  },
   updates: {
     fallbackToCacheTimeout: 0
   },
@@ -21,14 +15,9 @@ module.exports = {
     bundleIdentifier: 'com.amoo.datingapp'
   },
   android: {
-    adaptiveIcon: {
-      foregroundImage: './assets/adaptive-icon.png',
-      backgroundColor: '#8A2BE2'
-    },
     package: 'com.amoo.datingapp'
   },
   web: {
-    favicon: './assets/favicon.png',
     bundler: 'webpack'
   }
 }; 
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,45 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const path = require('path');
 
 module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(
-    {
-      ...env,
-      babel: {
-        dangerouslyAddModulePathsToTranspile: ['@react-navigation']
-      }
-    },
-    argv
-  );
+  const config = await createExpoWebpackConfigAsync(env, argv);
 
   config.resolve.alias = {
     ...config.resolve.alias,
     'react-native$': 'react-native-web',
+    'react-native/Libraries/ReactPrivate/ReactNativePrivateInterface': 'react-native-web/dist/index',
+    'react-native/Libraries/ReactPrivate/ReactNativePrivateInitializeCore': 'react-native-web/dist/index',
+    '../Components/AccessibilityInfo/legacySendAccessibilityEvent': 'react-native-web/dist/modules/AccessibilityInfo',
+    '@react-native/assets/registry': 'react-native-web/dist/modules/AssetRegistry',
+    '../Utilities/Platform': 'react-native-web/dist/exports/Platform',
+    './Platform': 'react-native-web/dist/exports/Platform',
+    'react-native/Libraries/Utilities/Platform': 'react-native-web/dist/exports/Platform'
   };
 
-  config.resolve.extensions = [
-    '.web.js',
-    '.web.jsx',
-    '.web.ts',
-    '.web.tsx',
-    ...config.resolve.extensions,
-  ];
+  config.module = {
+    ...config.module,
+    rules: [
+      ...config.module.rules,
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        include: [
+          path.resolve(__dirname, 'node_modules/@react-navigation'),
+          path.resolve(__dirname, 'node_modules/react-native-reanimated'),
+          path.resolve(__dirname, 'node_modules/react-native-maps'),
+          path.resolve(__dirname, 'node_modules/react-native-screens'),
+          path.resolve(__dirname, 'node_modules/react-native-safe-area-context'),
+          path.resolve(__dirname, 'node_modules/@react-native-community'),
+          path.resolve(__dirname, 'node_modules/react-native-gesture-handler')
+        ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  };
 
   return config;
 }; 
